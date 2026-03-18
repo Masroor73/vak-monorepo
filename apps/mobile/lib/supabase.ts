7ac7faf58f861351477f9c595513e1a81180219a
ZainabTameem <Tameemz2005@gmail.com>
2026-03-17 21:59:06 -0600

    Update supabase.ts

--- apps/mobile/lib/supabase.ts
+++ apps/mobile/lib/supabase.ts
@@ -2,8 +2,8 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
 const memoryStorage = {
   getItem: async () => null,
